760b681b7f47218a17799d5b2e5e8894d458d2c9
clipnest <clipnest3@gmail.com>
2026-02-01 16:03:23 +0530

    Update license.js

--- js/license.js
+++ js/license.js
@@ -1,20 +1,22 @@
 const KEYS=[
- "CLIP-7A92-X1",
- "CLIP-4K88-Z9",
- "CLIP-9P31-Q7",
- "CLIP-2M55-R4",
- "CLIP-8T63-L2"
+ "REVOX-7A92-X1",
+ "REVOX-4K88-Z9",
+ "REVOX-9P31-Q7",
+ "REVOX-2M55-R4",
+ "REVOX-8T63-L2"
 ];
 
 function checkKey(){
- if(KEYS.includes(keyInput.value)){
-  localStorage.setItem("licensed","true");
+ if(KEYS.includes(keyInput.value.trim())){
+  localStorage.setItem("licensed","yes");
   lock.classList.add("hidden");
   app.classList.remove("hidden");
- }else msg.innerText="Invalid license key";
+ }else{
+  msg.innerText="Invalid license key";
+ }
 }
 
-if(localStorage.getItem("licensed")){
+if(localStorage.getItem("licensed")==="yes"){
  lock.classList.add("hidden");
  app.classList.remove("hidden");
 }
